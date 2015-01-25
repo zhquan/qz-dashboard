ab51734b129524614056e59dcf0bb65ce9cd91c8
Quan Zhou <zhquan7@gmail.com>
2015-01-25 03:26:31 +0100

    date, tipo, color

--- js/index.js
+++ js/index.js
@@ -97,11 +97,27 @@ $(document).ready(function(){
 function json(){
     $.getJSON("json/dom-evolutionary.json").success(function(data){
         $.each( data, function( key, val ) {
-            $("#algo").append('<input type="checkbox" name="'+key+'" value="'+key+'">'+key+'<br>')
-            console.log(key);
-            console.log(val);
+            if (key != 'date'){
+                $("#algo").append('<input type="checkbox" name="'+key+'" value="'+key+'">'+key+'<br>')
+            }
         });
-        $("#algo").append('<input type="button" onclick="selection()" value="OK">');
+        $("#algo").append('<form name="formul"><label>Desde: </label><select id="desde" name="desde"></select></form>');
+        $.each( data, function( key, val ) {
+            if (key == 'date'){
+                for (var i = 0; i<val.length; i++){
+                    $("#desde").append('<option value="'+val[i]+'">'+val[i]+'</option>');
+                }
+            }
+        });
+        $("#algo").append('<form name="formul2"><label>Hasta: </label><select id="hasta" name="hasta"></select>');
+        $.each( data, function( key, val ) {
+            if (key == 'date'){
+                for (var i = 0; i<val.length; i++){
+                    $("#hasta").append('<option value="'+val[i]+'">'+val[i]+'</option>');
+                }
+            }
+        });
+        $("#algo").append('<label>Tipo: </label><form name="formul3"><select name="tipo"><option value="Line">Line</option><option value="Bar">Bar</option><option value="Radar">Radar</option></select></form><br><label>Color: </label><input type="text" value="yellow" id="color"></input><br><input type="button" onclick="selection()" value="OK">');
     });
 }
 function selection() {
@@ -109,6 +125,7 @@ function selection() {
     $("#algo input[type='checkbox']:checked").each(function(){
         check.push($(this).attr('value'));
     })
+    var color = document.getElementById('color').value;
     var valor = sacar(check);
     console.log('checked '+check);
     var Datas = {
@@ -116,10 +133,10 @@ function selection() {
 	    datasets : [
 		    {
                 label: check,
-			    fillColor : "yellow",
-			    strokeColor : "yellow",
-			    highlightFill: "yellow",
-			    highlightStroke: "yellow",
+			    fillColor : color,
+			    strokeColor : color,
+			    highlightFill: color,
+			    highlightStroke: color,
 			    data : valor[1]
 		    }
         ]
@@ -135,39 +152,53 @@ function selection() {
                 +'<% } %>'
               +'</ul>'
     }
-    var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(Datas, options,{
-		responsive: true
-	});
+    var tipo = document.formul3.tipo.options[document.formul3.tipo.selectedIndex].value;
+    if (tipo == 'Line'){
+        var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(Datas, options);
+    }else if (tipo == 'Bar'){
+        var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Bar(Datas, options);
+    }else if (tipo == 'Radar'){
+        var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Radar(Datas, options);
+    }
     var legend = myLine.generateLegend();
     $("#legend").append(legend);
 }
 function sacar(check) {
+    var desde = document.formul.desde.options[document.formul.desde.selectedIndex].value;
+    var hasta = document.formul2.hasta.options[document.formul2.hasta.selectedIndex].value;
     var pos = [];
     var tiempo = [];
     var dato1 = [];
+    var entrar = false;
     $.ajax({
         url: 'json/dom-evolutionary.json',
         dataType: 'json',
         async: false,
         success: function(json){
-            //Para coger el array checkeado
-            $.each( json, function( key, val ) {
-                for (var j = 0; j < check.length; j++){
-                    if (check[j] == key){
-                        for (var i = 0; i < val.length; i++){
-                            if (val[i] != 0){
-                                pos.push(i);
-                                dato1.push(val[i]);
-                            }
+            //Su correspondiente fecha
+            $.each( json, function ( key, val ){
+                if (key == 'date'){
+                    for(var i = 0; i < val.length; i++){
+                        if(val[i] == desde){
+                            entrar=true;
+                        }
+                        if(entrar){
+                            pos.push(i);
+                            tiempo.push(val[i]);
+                        }
+                        if(val[i] == hasta){
+                            entrar=false;
                         }
                     }
                 }
             });
-            //Su correspondiente fecha
-            $.each( json, function ( key, val ){
-                if ((key == 'date') && (pos.length>0)){
-                    for(var i = 0; i < pos.length; i++){
-                        tiempo.push(val[pos[i]]);
+            //Rellenar el array de dato
+            $.each( json, function( key, val ) {
+                for (var j = 0; j < check.length; j++){
+                    if (check[j] == key){
+                        for (var i = 0; i < pos.length; i++){
+                            dato1.push(val[pos[i]]);
+                        }
                     }
                 }
             });
